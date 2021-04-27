2fa81a2a27c6a1b5463b9d66e778bf1ba143565a
mufaddal <mufaddalmm.52@gmail.com>
2021-04-27 15:49:38 +0530

    CTA

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import AppBar1 from './../components/HomePage/AppBar'
 import TopPost from './../components/HomePage/TopPost'
+import CTA from './../components/HomePage/CTA'
 import React from 'react'
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       </Head>
 <AppBar1/>
 <TopPost/>
+<CTA/>
     </>
   )
 }
