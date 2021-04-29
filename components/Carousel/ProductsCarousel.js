910e268687809d00e29af6a920a8cf9086180183
mufaddal <mufaddalmm.52@gmail.com>
2021-04-29 16:22:55 +0530

    arrows

--- components/Carousel/ProductsCarousel.js
+++ components/Carousel/ProductsCarousel.js
@@ -26,10 +26,10 @@ const useStyles = makeStyles((theme) => ({
         display: 'flex',
         marginRight: '40px',
         [theme.breakpoints.down('sm')]: {
-            float:'none',
-            margin:'auto',
-        marginRight: 'auto',
-        justifyContent: 'center',
+            float: 'none',
+            margin: 'auto',
+            marginRight: 'auto',
+            justifyContent: 'center',
         },
     },
     dots: {
@@ -37,32 +37,50 @@ const useStyles = makeStyles((theme) => ({
         display: 'flex',
         marginLeft: '40px',
         [theme.breakpoints.down('sm')]: {
-            display:'none'
+            display: 'none'
         },
     },
     arrowPrev: {
         margin: '2px',
         marginRight: '30px',
-        height:'fit-content',
-        cursor:'pointer'
+        height: 'fit-content',
+        cursor: 'pointer'
     },
     arrowNext: {
         margin: '2px',
-        height:'fit-content',
-        cursor:'pointer'
+        height: 'fit-content',
+        cursor: 'pointer'
     },
     dotPrev: {
         margin: '2px',
         marginRight: '12px',
-        height:'fit-content',
-        cursor:'pointer'
+        height: 'fit-content',
+        cursor: 'pointer'
     },
     dotNext: {
         margin: '2px',
-        height:'fit-content',
-        cursor:'pointer'
+        height: 'fit-content',
+        cursor: 'pointer'
+    },
+    activeDot: {
+        backgroundColor: '#000000',
+        height: '9px',
+        width: '9px',
+        borderRadius: '50%',
+    },
+    inactiveDot: {
+        background: '#fff',
+        height: '9px',
+        width: '9px',
+        borderRadius: '50%',
+        border: 'solid 1px #000000',
+    },
+    inactiveArrowNext: {
+        opacity:'0.5'
+    },
+    inactiveArrowPrev: {
+        opacity:'0.5'
     }
-
 }))
 
 export default function ProductCarousel(props) {
@@ -75,21 +93,27 @@ export default function ProductCarousel(props) {
     const gotoPrev = () => {
         sliderRef.current.slickPrev();
     }
+    const [updateCount, setUpdateCount] = useState(0);
+    const [slideIndex, setSlideIndex] = useState(0);
+    const handleOnClick = index => {
+        sliderRef.current.slickGoTo(index);
+    };
     const settings = {
         arrows: false,
         dots: false,
-        infinite: true,
+        infinite: false,
         speed: 500,
         slidesToShow: 3,
-        slidesToScroll: 1,
+        slidesToScroll: 3,
         initialSlide: 0,
+        afterChange: () => setUpdateCount(updateCount + 1),
+        beforeChange: (current, next) => setSlideIndex(next),
         responsive: [
             {
                 breakpoint: 973,
                 settings: {
                     slidesToShow: 2,
-                    slidesToScroll: 1,
-                    infinite: true,
+                    slidesToScroll: 2,
                     dots: false
                 }
             },
@@ -117,32 +141,30 @@ export default function ProductCarousel(props) {
     return (
         <div className={styles.container}>
             <Slider ref={sliderRef} {...settings}>
-                <ProductCard rating='4.5' name={`${props.name ? props.name :'Hair fall Control Shampoo ' } `} />
-                <ProductCard rating='4.5' name={`${props.name ? props.name :'Hair fall Control Shampoo ' } `} />
-                <ProductCard rating='4.5' name={`${props.name ? props.name :'Hair fall Control Shampoo ' } `} />
-                <ProductCard rating='4.5' name={`${props.name ? props.name :'Hair fall Control Shampoo ' } `} />
-                <ProductCard rating='4.5' name={`${props.name ? props.name :'Hair fall Control Shampoo ' } `} />
-                <ProductCard rating='4.5' name={`${props.name ? props.name :'Hair fall Control Shampoo ' } `} />
+                <ProductCard rating='4.5' name={`${props.name ? props.name : 'Hair fall Control Shampoo '} `} />
+                <ProductCard rating='4.5' name={`${props.name ? props.name : 'Hair fall Control Shampoo '} `} />
+                <ProductCard rating='4.5' name={`${props.name ? props.name : 'Hair fall Control Shampoo '} `} />
+                <ProductCard rating='4.5' name={`${props.name ? props.name : 'Hair fall Control Shampoo '} `} />
+                <ProductCard rating='4.5' name={`${props.name ? props.name : 'Hair fall Control Shampoo '} `} />
+                <ProductCard rating='4.5' name={`${props.name ? props.name : 'Hair fall Control Shampoo '} `} />
 
             </Slider>
 
             <div className={styles.bottom}>
                 <div className={styles.dots}>
-                   
-                <div onClick={gotoPrev} className={styles.dotPrev}>
-                        <img src='/dotop.png' />
+                    <div onClick={gotoPrev} className={styles.dotPrev}>
+                        <div className={`${slideIndex == 0 ? styles.activeDot : styles.inactiveDot} `} />
                     </div>
                     <div onClick={gotoNext} className={styles.dotNext}>
-                        <img src='/dottr.png' />
+                        <div className={`${slideIndex ? styles.activeDot : styles.inactiveDot} `} />
                     </div>
                 </div>
                 <div className={styles.arrows}>
-
                     <div onClick={gotoPrev} className={styles.arrowPrev}>
-                        <img src='/arrowprev.png' />
+                        <img className={`${slideIndex == 0 ? styles.inactiveArrowPrev : '' } `} src='/arrowprev.png' />
                     </div>
                     <div onClick={gotoNext} className={styles.arrowNext}>
-                        <img src='/arrownext.png' />
+                        <img src='/arrownext.png' className={`${slideIndex ? styles.inactiveArrowPrev : ''} `}/>
                     </div>
                 </div>
             </div>
